1f5874253428a00e5ad8c38cf505dd1743c91f01
John Anderson <john.i.anderson@gmail.com>
2020-08-20 15:34:35 -0500

    Initial Commit

---
+++ config.h
@@ -0,0 +1,14 @@
+// enter your latitude (example: North 40º33'20'')
+int latHH = 43;    // this means 40º North
+int latMM = 30;
+int latSS = 10.8;
+
+// enter Pole Star right ascention (AR: HH:MM:SS)
+int poleAR_HH = 2;    // this means 2 hours, 52 minutes and 16 seconds
+int poleAR_MM = 31;
+int poleAR_SS = 49;
+
+// enter Pole Star hour angle (H: HH:MM:SS)
+int poleH_HH = 8;
+int poleH_MM = 32;
+int poleH_SS = 7;
